e4fed880d2214e1144d5fc24be1d7836176f5147
dalepotter <dalepotter@gmail.com>
2018-05-24 17:39:50 +0100

    Add stub function defintion
    This defines the expected inputs and outputs of the function.

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Please refer to the information supplied by email for the full context of this r
 
 ## Developer setup
 ```
+
 ```
---
+++ src/main.py
@@ -0,0 +1,10 @@
+def get_total_budget_value(year):
+    """Compute the total USD value of projects for the input year.
+
+    Args:
+        year (int): A value corresponding to a year.
+
+    Returns:
+        (float) The USD value of projects that start in the given year
+    """
+    # Write your code here
